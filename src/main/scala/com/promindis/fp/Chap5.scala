c9911268d3058ddeda9411744ba85e153974a7f4
globulon <globulon@gmail.com>
2013-01-12 20:04:55 +0100

    chap5: stream basic hofs

--- src/main/scala/com/promindis/fp/Chap5.scala
+++ src/main/scala/com/promindis/fp/Chap5.scala
@@ -27,12 +27,46 @@ object Chap5 {
         case _ => Stream.empty[A]
       }
 
-    def takeWhile(p: A  => Boolean): Stream[A] =
+//    def takeWhile(p: A  => Boolean): Stream[A] =
+//      uncons match {
+//        case Some((h, t)) if p(h) => Stream.cons(h, t.takeWhile(p))
+//        case _ => Stream.empty
+//      }
+
+    def foldRight[B](z: B)(f: (A, => B) => B): B =
       uncons match {
-        case Some((h, t)) if p(h) => Stream.cons(h, t.takeWhile(p))
-        case _ => Stream.empty
+        case Some((h, t)) => f(h, t.foldRight(z)(f))
+        case _ => z
       }
 
+    def exists(p: A => Boolean): Boolean =
+      foldRight(false) { (a, b) => p(a) || b }
+
+    def forAll(p: A => Boolean): Boolean =
+      foldRight(true) { (a, b) => p(a) && b }
+
+    def takeWhile(p: A  => Boolean): Stream[A] =
+      foldRight(Stream.empty[A]) { (a, s) =>
+        if (p(a)) Stream.cons(a, s)
+        else s
+      }
+    //map
+    def map[B](f: A => B): Stream[B] =
+      foldRight(Stream.empty[B]) { (a, b) =>
+        Stream.cons(f(a),b)
+      }
+
+    def filter(p: A => Boolean): Stream[A] =
+      foldRight(Stream.empty[A]) { (a, s) =>
+        if (p(a)) Stream.cons(a, s)
+        else s
+      }
+
+    def append[B >: A](b: Stream[B]): Stream[B] =
+      foldRight(b) { (a, s) => Stream.cons(a, s) }
+
+    def flatMap[B](f: A => Stream[B]): Stream[B] =
+      foldRight(Stream.empty[B]) { (a, s) => f(a).append(s) }
   }
 
   object Stream {
