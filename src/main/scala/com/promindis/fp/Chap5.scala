61bacb7c5c970450fa2ce7731a16c245c7f902f0
globulon <globulon@gmail.com>
2013-01-13 17:48:09 +0100

    chap5: done

--- src/main/scala/com/promindis/fp/Chap5.scala
+++ src/main/scala/com/promindis/fp/Chap5.scala
@@ -39,6 +39,12 @@ object Chap5 {
         case _ => z
       }
 
+    def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+      foldRight((z, Stream(z))) { (a, p) =>
+        val r = f(a, p._1)
+        (r, Stream.cons(r, p._2))
+      }._2
+
     def exists(p: A => Boolean): Boolean =
       foldRight(false) { (a, b) => p(a) || b }
 
@@ -67,6 +73,48 @@ object Chap5 {
 
     def flatMap[B](f: A => Stream[B]): Stream[B] =
       foldRight(Stream.empty[B]) { (a, s) => f(a).append(s) }
+
+    def map_1[B](f: A => B): Stream[B] =
+      Stream.unfold(this) { (s) => s.uncons map(p => (f(p._1), p._2)) }
+
+    def take_1[B](n: Int): Stream[A] =
+      Stream.unfold((n, this)) { (s) =>
+        s._2.uncons match {
+          case Some((h, t)) if (s._1 > 0) => Some((h, (s._1 - 1, t)))
+          case _ => None
+      }
+     }
+
+    def takeWhile_1(p: A => Boolean): Stream[A] =
+      Stream.unfold(this) { s =>
+        s.uncons match {
+          case Some((h,t)) if p(h) => Some((h,t))
+          case _ => None
+        }
+      }
+
+    def zipWith[B, C](bs: Stream[B])(f: (A, B) => C): Stream[C] =
+      Stream.unfold((this, bs)) { s =>
+        (s._1.uncons, s._2.uncons) match {
+          case (Some((ha, ta)), Some((hb, tb))) => Some(f(ha, hb), (ta, tb))
+          case _ => None
+        }
+      }
+
+    def zip[B](bs: Stream[B]): Stream[(A, B)] = zipWith(bs)((_, _))
+
+    def zipWithAll[B, C](b: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+      Stream.unfold((Some(this): Option[Stream[A]], Some(b): Option[Stream[B]])) { s =>
+        s._1 flatMap { as =>
+          s._2.map { bs =>
+            (f(as.uncons.map(_._1), bs.uncons.map(_._1)), (as.uncons.map(_._2), bs.uncons.map(_._2)))
+          }
+        }
+      }
+
+    def zipAll[B](bs: Stream[B]): Stream[(Option[A], Option[B])] = zipWithAll(bs) ((_, _))
+
+    def tails: Stream[Stream[A]] = Stream.tails(this)
   }
 
   object Stream {
@@ -83,7 +131,46 @@ object Chap5 {
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty
       else cons(as.head, apply(as.tail: _*))
+
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case Some((a, zz)) => Stream.cons(a, unfold(zz)(f))
+        case _ => Stream.empty
+      }
+
+  def startsWith[A](s: Stream[A], s1: Stream[A]): Boolean =
+     !s.zipAll(s1).takeWhile(_._2.isDefined).exists { p => p._1 != p._2 }
+
+  def tails[A](stream: Stream[A]): Stream[Stream[A]] =
+    unfold(stream) { s =>
+      s.uncons.map (p => (s, p._2))
+    }
+
+  def hasSubsequence[A](s1: Stream[A], s2: Stream[A]): Boolean =
+    s1.tails exists (startsWith(_,s2))
+
   }
 
   val s = Stream(1,2,3)
+
+
+//  val ones: Stream[Int] = Stream.cons(1, ones)
+//
+//  def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
+//
+//  def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+//
+//  def makeFib(a: Int, b: Int): Stream[Int] = Stream.cons(a, makeFib(b, a + b))
+//
+//  val fibs = makeFib(0, 1)
+
+  def constant[A](a: A) : Stream[A] = Stream.unfold(a) { (s) => Some((s, s))}
+
+  val ones: Stream[Int] = constant(1)
+
+  def from(n: Int): Stream[Int] = Stream.unfold(n) { (s) => Some((s, s + 1)) }
+
+  val fibs: Stream[Int] = Stream.unfold((0, 1)) { (s) => Some((s._1, (s._2, s._1 + s._2))) }
+
+
 }
